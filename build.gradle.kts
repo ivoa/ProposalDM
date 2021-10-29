5d0e3ab48226064b1176a2c72f44ee3bb514ff39
Paul Harrison <paul.harrison@manchester.ac.uk>
2021-10-29 16:26:57 +0100

    put in place more of the build infrastructure
    The models themselves are still an early WIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,42 @@
 plugins {
         id("net.ivoa.vo-dml.vodmltools") version "0.1"   
 }
+
+group = "org.javastro.ivoa.vo-dml"
+version = "0.1-SNAPSHOT"
+
+
+vodml {
+        vodmlFiles.setFrom(project.files (
+                vodmlDir.file("proposaldm.vo-dml.xml"),
+                vodmlDir.file("proposalReview.vo-dml.xml"),
+                vodmlDir.file("IVOA-v1.0.vo-dml.xml"),
+                vodmlDir.file("STC_coords-v1.0.vo-dml.xml")
+        ))
+        bindingFiles.setFrom(
+                project.files(
+                        layout.projectDirectory.asFileTree.matching (
+                                PatternSet().include("binding*model.xml")
+                        )
+                )
+        )
+        outputDocDir.set(layout.projectDirectory.dir("std/generated"))
+}
+
+repositories {
+        mavenCentral()
+        mavenLocal() // TODO remove this when releasing - just here to pick up local vodml-runtime
+}
+
+dependencies {
+        implementation("org.javastro.ivoa.vo-dml:vodml-runtime:0.1")
+//    implementation("org.javastro:ivoa-entities:0.9.3-SNAPSHOT")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+
+        implementation("org.slf4j:slf4j-api:1.7.32")
+        testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
+
+        testImplementation("org.apache.derby:derby:10.14.2.0")
+
+}
