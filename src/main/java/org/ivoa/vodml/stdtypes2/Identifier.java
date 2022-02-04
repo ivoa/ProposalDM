753f7891e0d8d615300eae82ffad401749a6d18e
Paul Harrison <paul.harrison@manchester.ac.uk>
2022-02-04 17:53:18 +0000

    need specialization of more of ivoa base model
    this is needed because of the problems with the null support in hibernate
    now the whole of the ivoa base model is mapped to specializations rather than allow the automatic generation - might still have some knock-on effects re generation in other areas.

--- src/main/java/org/ivoa/vodml/stdtypes2/Identifier.java
+++ src/main/java/org/ivoa/vodml/stdtypes2/Identifier.java
@@ -21,41 +21,13 @@ import javax.persistence.*;
 
         private static final long serialVersionUID = 1L;
 
-        /**  representation */
-        @javax.xml.bind.annotation.XmlValue
-        private Object value;
-
-        /**
-         * Creates a new Identifier Primitive Type instance, wrapping a base type.
-         *
-         * @param v 
-         */
-        public Identifier(final Object v) {
-            this.value = v;
-        }
-        /**
+       /**
          * no arg constructor.
          */
         protected Identifier() {}
 
-        /**
-         * Return the representation of this primitive (value)
-         * @return string representation of this primitive( value)
-         */
-        public final Object value() {
-            return this.value;
-        }
+ 
 
-        /**
-         * Return the string representation of this primitive value
-         * @see #value()
-         * @return string representation of this primitive
-         */
-        @Override
-        public final String toString() {
-            return value().toString();
-        }
-              
 
       }
   
