0c35a088a61e2725c775531342eac16dc1bc2294
Paul Harrison <paul.harrison@manchester.ac.uk>
2023-04-04 21:02:04 +0100

    add the snapshot repository
    so that the IVOA Base model can  be picked up

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,5 +12,12 @@ dependencyResolutionManagement {
     repositories {
         mavenLocal()
         mavenCentral()
+        /*
+        add this repository to pick up the SNAPSHOT version of the IVOA base library - in the future when this
+        will not be necessary when this library is released as a non-SNAPSHOT version.
+         */
+        maven {
+            url= uri("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
     }
 }
