af512b765dbd2ff7d0d61cdb57c4a329efaf3268
Paul Harrison <paul.harrison@manchester.ac.uk>
2023-07-10 15:57:54 +0100

    use latest version of vodmltools plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.kotlin.dsl.accessors.runtime.addDependencyTo
 
 plugins {
-        id("net.ivoa.vo-dml.vodmltools") version "0.3.23"
+        id("net.ivoa.vo-dml.vodmltools") version "0.3.24"
         `maven-publish`
         id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
         signing
