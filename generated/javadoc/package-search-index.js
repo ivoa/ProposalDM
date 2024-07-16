f418884be8a75e2ec11c74a89524986d530a23ab
Paul Harrison <paul.harrison@manchester.ac.uk>
2024-07-16 11:01:08 +0100

    Deployed 89d8968 with MkDocs version: 1.6.0

--- generated/javadoc/package-search-index.js
+++ generated/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.ivoa.dm.proposal.management"},{"l":"org.ivoa.dm.proposal.prop"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.ivoa.dm.proposal.management"},{"l":"org.ivoa.dm.proposal.prop"}];updateSearchResults();
